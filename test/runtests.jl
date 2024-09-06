11d7f105f2d9975c3d59d6d9d42f13034bd021b1
ocots <66357348+ocots@users.noreply.github.com>
2024-09-06 00:20:24 +0000

    :robot: Format .jl files

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,10 +4,7 @@ using CTApp
 
 #
 @testset verbose = true showtiming = true "CTApp tests" begin
-    for name ∈ (
-        :aqua,
-        :default,
-        )
+    for name in (:aqua, :default)
         @testset "$(name)" begin
             test_name = Symbol(:test_, name)
             include("$(test_name).jl")
